70c2bbbad922693fc95f8592da76fe4e5e70bcd6
Bela <122638570+belagrf@users.noreply.github.com>
2026-05-13 21:57:44 +0200

    Use QVariant userType in mpv helper

--- ci/check_modern_qt_helpers.sh
+++ ci/check_modern_qt_helpers.sh
@@ -12,3 +12,8 @@ if rg -n '\b(NULL|Q_NULLPTR)\b' "$repo_dir/src"; then
     printf 'src must use nullptr/FALSE instead of legacy null macros.\n' >&2
     exit 1
 fi
+
+if rg -n 'QVariant::Type|\.type\(\)' "$repo_dir/src/player/qthelper.hpp"; then
+    printf 'qthelper.hpp must use QVariant::userType() for Qt meta-type checks.\n' >&2
+    exit 1
+fi
